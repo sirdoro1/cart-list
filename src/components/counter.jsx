37ac29d3cce1d35450943f520d6639b7b91a7e1b
Dave Doro <sirdoro1@gmail.com>
2021-03-08 01:26:02 +0100

    update on testing

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+
+    }
+
+    render() { 
+        return (
+        <h1>Hello World</h1>
+        );
+    }
+}
+ 
+export default Counter;
