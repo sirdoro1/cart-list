c436dff7adbe34af52a8829af3e46c4fcf18c6f0
Dave Doro <sirdoro1@gmail.com>
2021-03-10 23:32:03 +0100

    update on styling of elements

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,11 +5,17 @@ class Counter extends Component {
         count: 0,
         agenda: 2,
     }
+
+     style = {
+         fontSize:20,
+         fontWeight:'bold',
+     };
+
     render() { 
         return (
             <div>
-                <span className="badge badge-primary m-2">{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button> 
+                <span style={this.style} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button style={{fontSize:10, fontWeight:'bold'}} className="btn btn-secondary btn-sm">Increment</button> 
             </div>
         );
     }
